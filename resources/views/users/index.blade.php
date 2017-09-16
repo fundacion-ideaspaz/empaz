ee0b804cd8655089c4c7ecc50de2a632000cf6b8
Santiago Mendoza Ramirez <santiagomendozar@gmail.com>
2017-09-16 12:00:18 -0500

    Add validation erros. Convert cargo input in a select.

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,27 +1,28 @@
-@extends('layouts.master')
-
-@section('title', 'Crear usuario')
-
-@section('content')
-  <h2>Lista de Usuarios</h2>
-
-  <table class="table">
-  <thead>
-    <tr>
-      <th>Nombre</th>
-      <th>Cargo</th>
-      <th>Email</th>
-    </tr>
-  </thead>
-  <tbody>
-  @foreach($users as $user)
-    <p></p>
-    <tr>
-      <td>{{$user->nombre}}</td>
-      <td>{{$user->cargo}}</td>
-      <td>{{$user->correo}}</td>
-    </tr>
-  @endforeach
-  </tbody>
-</table>
+@extends('layouts.master') @section('title', 'Panel de Usuarios') @section('content')
+<div class="row">
+  <div class="col-12 card">
+    <div class="card-body">
+      <h2>Lista de Usuarios</h2>
+      <table class="table table-striped table-hover">
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Cargo</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          @foreach($users as $user)
+          <p></p>
+          <tr>
+            <td>{{$user->nombre}}</td>
+            <td>{{$user->cargo}}</td>
+            <td>{{$user->correo}}</td>
+          </tr>
+          @endforeach
+        </tbody>
+      </table>
+    </div>
+  </div>
+</div>
 @endsection
