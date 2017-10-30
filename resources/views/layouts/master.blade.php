4cc4d5de7574a5dc54345a56b4f6cdf2f23c6ce6
Christian Benito <bbull.inc@gmail.com>
2017-10-30 12:52:54 -0500

    Cambios Front

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -11,8 +11,8 @@
     crossorigin="anonymous">
   <link rel="stylesheet" href="/css/bootstrap-slider.css">
   <link href="/css/multi-select.css" media="screen" rel="stylesheet" type="text/css">
-  <link rel="stylesheet" href="css/styles.css">
-  <link rel="stylesheet" href="css/font-awesome.min.css">
+  <link rel="stylesheet" href="/css/styles.css">
+  <link rel="stylesheet" href="/css/font-awesome.min.css">
 
 </head>
 
