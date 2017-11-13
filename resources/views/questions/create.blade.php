b59ed317e46bffce8083ecc80667fec3abbddf63
Christian Benito <bbull.inc@gmail.com>
2017-11-13 13:08:37 -0500

    prueba

--- resources/views/questions/create.blade.php
+++ resources/views/questions/create.blade.php
@@ -93,16 +93,6 @@
                 $("#respuesta_4").attr("disabled", "false");
                 $("#respuesta_3").attr("disabled", "false");
                 var formWrap = document.getElementById( 'fs-form-wrap' );
-
-        [].slice.call( document.querySelectorAll( 'select.cs-select' ) ).forEach( function(el) {  
-          new SelectFx( el, {
-            stickyPlaceholder: false,
-            onChange: function(val){
-              document.querySelector('span.cs-placeholder').style.backgroundColor = val;
-            }
-          });
-        } );
-
         new FForm( formWrap, {
           onReview : function() {
             classie.add( document.body, 'overview' ); // for demo purposes only
