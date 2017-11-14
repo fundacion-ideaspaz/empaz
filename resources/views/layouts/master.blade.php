b809cf5cdc3cfc6d3d9f5922c8c3a2e1b209d1ba
Christian Benito <bbull.inc@gmail.com>
2017-11-14 13:24:05 -0500

    prueba

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -73,12 +73,16 @@
     @yield('content')
   </div>
 </body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script src="/popper/umd/popper.js"></script>
 <script src="/js/bootstrap-slider.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1"
   crossorigin="anonymous"></script>
 <script src="/js/jquery.multi-select.js" type="text/javascript"></script>
 <script type="text/javascript">
-  $('#example').tooltip(options)
+$( document ).ready(function() {
+    $('#example').tooltip(options);
+});
+  
 </script>
 </html>
