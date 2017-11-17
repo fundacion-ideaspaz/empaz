c70065a04676499a801b533b5a81d2a2b11850cf
Christian Benito <bbull.inc@gmail.com>
2017-11-17 06:41:33 -0500

    prueba

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -26,10 +26,9 @@
                         {{ csrf_field() }}
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
 
                             <div class="col-md-10">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+                                <input id="email" type="email" class="form-control" placeholder="Email" name="email" value="{{ old('email') }}" required autofocus>
 
                                 @if ($errors->has('email'))
                                     <span class="help-block">
@@ -40,10 +39,9 @@
                         </div>
 
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
 
                             <div class="col-md-10">
-                                <input id="password" type="password" placeholder="Email" class="form-control" name="password" required>
+                                <input id="password" type="password" placeholder="Contraseña" class="form-control" name="password" required>
 
                                 @if ($errors->has('password'))
                                     <span class="help-block">
