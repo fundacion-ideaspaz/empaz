2ae5e172a6047bb453cb61fb6f68d112b0a817a6
Christian Benito <bbull.inc@gmail.com>
2017-11-17 07:50:02 -0500

    prueba

--- resources/views/layouts/masterAnimation.blade.php
+++ resources/views/layouts/masterAnimation.blade.php
@@ -56,6 +56,84 @@
   <div>
     @yield('content')
   </div>
+  <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      <div class="row">
+      <div class="col-md-5 derecha">
+        <h3>Registrar nueva empresa</h3>
+         <a href="/registro">Crear nueva empresa</a>
+      </div>
+      <div class="col-md-7">
+      <h3>Ingresar</h3>
+         <div class="panel panel-default">
+
+                <div class="panel-body">
+                    <form class="form-horizontal" method="POST" action="{{ route('login') }}">
+                        {{ csrf_field() }}
+
+                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+
+                            <div class="col-md-10">
+                              <i class="fa fa-user" aria-hidden="true"></i>
+                                <input id="email" type="email" class="form-control" placeholder="Email" name="email" value="{{ old('email') }}" required autofocus>
+
+                                @if ($errors->has('email'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+
+                            <div class="col-md-10">
+                              <i class="fa fa-unlock-alt" aria-hidden="true"></i>
+                                <input id="password" type="password" placeholder="Contraseña" class="form-control" name="password" required>
+
+                                @if ($errors->has('password'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group" id="contentemail">
+                            <div class="col-md-6 col-md-offset-4">
+                                <div class="checkbox">
+                                    <label>
+                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-10">
+                                <button type="submit" class="btn btn-primary">
+                                    Ingresar
+                                </button>
+
+                                <a class="recuperarpass" href="{{ route('password.request') }}">
+                                    Forgot Your Password?
+                                </a>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div></div></div>
+      </div>
+    </div>
+  </div>
+</div> 
 </body>
 <script src="/popper/umd/popper.js"></script>
 <script src="/js/bootstrap-slider.js"></script>
