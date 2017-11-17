f525b89004f02b05e68c1281d13bb5ad8c24c833
Santiago Mendoza Ramirez <smendoza@isf.com.co>
2017-11-17 08:27:42 -0500

    Remove commented code

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -246,26 +246,3 @@
   </div>
  </div> 
 @endsection
-
-<!-- <div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                    @if(Auth::user()->role === 'superadmin')
-                        <a href="/users" class="btn btn-primary">Users List</a>
-                        <a href="/dimensiones" class="btn btn-primary">Dimensiones List</a>
-                    @endif
-                </div>
-            </div>
-        </div>
-    </div>
-</div> -->
-
