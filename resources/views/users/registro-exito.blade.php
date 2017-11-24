5343b9193bc183828cff11c3c3198df8ac120f55
Santiago Mendoza Ramirez <smendoza@isf.com.co>
2017-11-24 13:35:40 -0500

    Confirmacion de registro exitoso.

---
+++ resources/views/users/registro-exito.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.master') @section('title', 'Panel de Usuarios') @section('content')
+<div class="row">
+  <div class="col-12 card">
+    <div class="card-body">
+      <h2>Registro exitoso</h2>
+      <p>A tu correo hemos enviado un link de activación. Por favor, revisa tu correo
+        y activa tu cuenta para poder utilizar la plataforma de EmPaz.
+      </p>
+    </div>
+  </div>
+</div>
+@endsection
