7dbd043a95350ca5b6e3482a7c91db1cc9573fb2
Santiago Mendoza Ramirez <smendoza@isf.com.co>
2017-11-28 15:21:17 -0500

    Fix tipos de preguntas values

--- app/OpcionesRespuestas.php
+++ app/OpcionesRespuestas.php
@@ -16,13 +16,23 @@ class OpcionesRespuestas extends Model
 
     public function valueRespuesta()
     {
+        $tipoRespuesta = $this->pregunta->tipo_respuesta;
         switch ($this->number) {
             case 1:
                 return 5;
             case 2:
+                if($tipoRespuesta == 3){
+                    return 1;
+                }
+                if($tipoRespuesta == 4){
+                    return 1;
+                }
                 return 3;
             case 3:
-                return 2;
+                if($tipoRespuesta == 1){
+                    return 2;
+                }
+                return 1;
             case 4:
                 return 1;
             case -1:
@@ -30,5 +40,9 @@ class OpcionesRespuestas extends Model
             case -2:
                 return 1;
         }
+    }
+
+    public function pregunta(){
+        return $this->belongsTo("\App\Pregunta");
     }
 }
