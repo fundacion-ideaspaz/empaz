835b0bf36b61d5040ba34c5e6c9b2b05bbd96f25
Christian Benito <bbull.inc@gmail.com>
2017-11-29 05:48:30 -0500

    pruebas

--- resources/views/users/created.blade.php
+++ resources/views/users/created.blade.php
@@ -1,17 +1,17 @@
 <table width="650" align="center">
 <tr>
 	<td style="background-color: #00aabf; padding: 15px;">
-		<img src="http://empazweb.org/img/logo-b.svg">
+		<img src="http://104.131.34.207/img/logo-b.png">
 	</td>
 </tr>
 <tr>
 	<td style="background-color: #f2f2f2; padding: 20px;">
-		<h2 style="color: #666; text-align: center;">Hola, {{$user->nombre}}</h2>
+		<h2 style="color: #666;">Hola, {{$user->nombre}}</h2>
   <p style="color: #666;">
     Han creado una cuenta para ti en el proyecto Empaz. Para activar tu cuenta, da click en el siguiente enlace:
   </p>
   <p>
-    <a style="color: #fff; font-size: 16px; display: block; text-align: center; width: 200px; background-color: #00aabf; padding: 10px 20px; text-decoration: none;" href="{{URL::to('/')}}/users/{{$user->id}}/activate/{{$user->confirmation_code}}">Activar Cuenta</a>
+    <a style="color: #fff; font-size: 16px; display: block; text-align: center; width: 200px; margin:auto; background-color: #00aabf; padding: 10px 20px; text-decoration: none;" href="{{URL::to('/')}}/users/{{$user->id}}/activate/{{$user->confirmation_code}}">Activar Cuenta</a>
   </p>
   <p style="color: #666;">Luego deberas ingresar a tu cuenta con el usuario y contraseña que te hayan otorgado</p>
 
