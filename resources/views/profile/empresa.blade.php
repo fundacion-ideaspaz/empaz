bb97ae69703098a547358f23e05a1a25124b5e3e
Christian Benito <bbull.inc@gmail.com>
2017-11-29 07:20:55 -0500

    pruebas

--- resources/views/profile/empresa.blade.php
+++ resources/views/profile/empresa.blade.php
@@ -4,8 +4,8 @@
         <div class="card-body">
             <div class="row header">
             <div class="col-md-2"><i class="fa fa-address-card" aria-hidden="true"></i></div>
-            <div class="col-md-7"><h1>{{$empresa->nombre}}</h1></div>
-            <div class="col-md-3"><a class="btn btn-primary" href="/responder">Ver cuestionarios</a>
+            <div class="col-md-8"><h1>{{$empresa->nombre}}</h1></div>
+            <div class="col-md-2"><a class="btn btn-primary" href="/responder">Ver cuestionarios</a>
         </div></div>
             </div>
             <div class="row cuerpo">
