c4d61a3c0044d2bee80e4e334f05c9106efa5e68
Christian Benito <bbull.inc@gmail.com>
2017-11-29 07:49:20 -0500

    pruebas

--- resources/views/profile/user.blade.php
+++ resources/views/profile/user.blade.php
@@ -1,19 +1,28 @@
 @extends('layouts.master') @section('title', 'Perfil Empresa') @section('content')
 <div class="row indicadores-form">
   <div class="card col-12">
-    <div class="card-body">
-      <h1>{{Auth::user()->nombre}}</h1>
-      <ul>
-        <li>Role: {{ Auth::user()->role }}</li>
-        <li>Email: {{ Auth::user()->email }}</li>
-      </ul>
-      @if(Auth::user()->role === 'superadmin')
+        <div class="card-body">
+            <div class="row header">
+            <div class="col-md-2"><i class="fa fa-address-card" aria-hidden="true"></i></div>
+            <div class="col-md-8"><h1>{{Auth::user()->nombre}}</h1></div>
+            <div class="col-md-2"> @if(Auth::user()->role === 'superadmin')
       <a class="btn btn-primary" href="/users">Panel de administración</a>
       @endif
       @if(Auth::user()->role === 'experto')
       <a class="btn btn-primary" href="/cuestionarios">Panel de administración</a>
       @endif
+        </div></div>
+            </div>
+            <div class="row cuerpo">
+            <div class="col-md-6">
+                <span class="label">Role:</span> {{ Auth::user()->role }}
+            </div>
+            <div class="col-md-6">
+                <span class="label">Email:</span> {{ Auth::user()->email }}
+            </div>
+    
+            </div>
+            
     </div>
-  </div>
 </div>
 @endsection
