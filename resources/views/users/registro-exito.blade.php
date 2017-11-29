3dcca6e5bdc73099b02f3ba46fa8f04d7b1af4db
Christian Benito <bbull.inc@gmail.com>
2017-11-29 05:44:55 -0500

    pruebas

--- resources/views/users/registro-exito.blade.php
+++ resources/views/users/registro-exito.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.master') @section('title', 'Panel de Usuarios') @section('content')
 <div class="row">
-  <div class="col-12 card">
-    <div class="card-body registro-exitoso">
+  <div class="col-12 card registro-exitoso">
+    <div class="card-body">
       <h2>Registro exitoso</h2>
       <p>A tu correo hemos enviado un link de activación. Por favor, revisa tu correo
         y activa tu cuenta para poder utilizar la plataforma de EmPaz.
