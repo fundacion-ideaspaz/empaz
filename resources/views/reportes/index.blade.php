928183e652253d6a354008973c966bb91f4650bb
Christian Benito <bbull.inc@gmail.com>
2017-12-11 11:50:50 -0500

    pruebas

--- resources/views/reportes/index.blade.php
+++ resources/views/reportes/index.blade.php
@@ -1,6 +1,13 @@
 @extends('layouts.master') @section('title', 'Eliminar Pregunta') @section('content')
 <script type="text/javascript">
-var dimensiones = '{$importancia}}';
+    var locations = [
+
+    @foreach($rImportancia as $i=>$importancia)
+   {{ $dimensiones[$i]->nombre }}
+             {{ $dimensiones[$i]->descripcion}}
+           {{$importancia}}
+           @endforeach
+    ];
 
     console.log(dimensiones);
     console.log(locations);
