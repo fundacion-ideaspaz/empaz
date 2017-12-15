b9be59819c8100fac24af1164de25cd164e47384
Santiago Mendoza Ramirez <smendoza@isf.com.co>
2017-12-15 14:26:18 -0500

    Add Dummy PDF to manual de usuario

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -35,7 +35,7 @@
           <div class="collapse navbar-collapse" id="navbarNavDropdown">
               <ul class="navbar-nav">
                     <li class="nav-item">
-                      <a class="nav-link" href="#">Manual de usuario</a>
+                      <a class="nav-link" href="/dummypdf.pdf" target="_blank">Manual de usuario</a>
                     </li>
 
                     <li class="nav-item">
