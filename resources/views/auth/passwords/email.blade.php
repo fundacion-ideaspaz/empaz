be3c59a09a83b8bcd8f54e4ff7376f2e0c4d1ebd
Christian Benito <bbull.inc@gmail.com>
2017-12-20 14:27:55 -0500

    pruebas

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -5,7 +5,8 @@
     <div class="row">
         <div class="col-md-12">
             <div class="panel panel-default">
-                <div class="panel-heading">Recuperar contraseña</div>
+                <div class="panel-heading">
+                <h1>Recuperar contraseña</h1></div>
 
                 <div class="panel-body">
                     @if (session('status'))
