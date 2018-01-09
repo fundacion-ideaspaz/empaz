61bb8efceecadb4f13c2ec8a924a5b1884594289
Christian Benito <bbull.inc@gmail.com>
2018-01-09 09:01:03 -0500

    ultimos ajustes

--- resources/views/layouts/masterAnimationCues.blade.php
+++ resources/views/layouts/masterAnimationCues.blade.php
@@ -22,30 +22,69 @@
 
 <body>
 <header class="master">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="/login"><img src="/img/logo-b.svg" width="130"></a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"><i class="fa fa-bars" aria-hidden="true"></i></span>
-  </button>
+          <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <a class="navbar-brand" href="/home">
+            <img src="/img/logo-b.svg" width="130">
+          </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown"
+            aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"><i class="fa fa-bars" aria-hidden="true"></i></span>
+        </button>
 
-  <div class="collapse navbar-collapse" id="navbarNavDropdown">
-    <ul class="navbar-nav">
-      <li class="nav-item">
+          <div class="collapse navbar-collapse" id="navbarNavDropdown">
+              <ul class="navbar-nav">
+                    <li class="nav-item">
                       <a class="nav-link" href="/dummypdf.pdf" target="_blank">Manual de usuario</a>
                     </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/faq">Preguntas frecuentes</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/glosario">Glosario</a>
-      </li>
-    </ul>
-    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-  <i class="fa fa-user" aria-hidden="true"></i> Ingresar
-</button>
-</div> 
 
-</nav>
+                    <li class="nav-item">
+                      <a class="nav-link" href="/faq">FAQs</a>
+                    </li>
+
+                    <li class="nav-item">
+                      <a class="nav-link" href="/glosario">Glosario</a>
+                    </li>
+
+                    @if(Auth::user())
+                    <li class="nav-item dropdown">
+                          <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true"
+                            aria-expanded="false"><i class="fa fa-user-circle-o" aria-hidden="true"></i> Acciones de usuario
+                          </a>
+                          <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                              <a class="dropdown-item" href="{{ Auth::user()->role === 'empresa' ? '/profile/empresa' : '/profile/user' }}">Perfil</a>
+                              @if(Auth::user()->role === 'superadmin')
+                              <div class="menu-usuarios">
+                                  <a class="dropdown-item" href="/users"><i class="fa fa-user" aria-hidden="true"></i> Usuarios</a>
+                              </div>
+                              @endif
+                               @if(Auth::user()->role === 'experto' || Auth::user()->role === 'superadmin')
+                                  <a class="dropdown-item" href="/cuestionarios"><i class="fa fa-list-ul" aria-hidden="true"></i> Cuestionario</a>
+                                  <a class="dropdown-item" href="/dimensiones"><i class="fa fa-pie-chart" aria-hidden="true"></i> Dimensiones</a>
+                                  <a class="dropdown-item" href="/indicadores"><i class="fa fa-area-chart" aria-hidden="true"></i> Indicadores</a>
+                                  <a class="dropdown-item" href="/preguntas"><i class="fa fa-question-circle" aria-hidden="true"></i></i> Preguntas</a>
+                              @endif
+                              @if(Auth::user()->role === 'empresa')
+                              <a class="dropdown-item" href="/responder"><i class="fa fa-list-ul" aria-hidden="true"></i> Cuestionario</a>
+                              @endif
+                              @if(Auth::user()->role === 'consulta' || Auth::user()->role === 'superadmin')
+                              <a class="dropdown-item" href="/dashboard"><i class="fa fa-tachometer" aria-hidden="true"></i></i></i> Resultaods</a>
+                              @endif
+                              <div class="menu-logout">
+                                  <a class="logout-link dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                  document.getElementById('logout-form').submit();"><i class="fa fa-lock" aria-hidden="true"></i> 
+                                  Cerrar sesión
+                                  </a>
+                                  <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                      {{ csrf_field() }}
+                                  </form>
+                              </div>
+
+                          </div>
+                    </li>
+                    @endif
+              </ul>
+          </div>
+    </nav>
 </header>
   <div>
     @yield('content')
