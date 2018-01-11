20637b5f020ec32bb2e213eb46fbc87048354722
Christian Benito <bbull.inc@gmail.com>
2018-01-10 21:08:04 -0500

    últimos ajustes

--- resources/views/vendor/mail/html/layout.blade.php
+++ resources/views/vendor/mail/html/layout.blade.php
@@ -24,6 +24,11 @@
     </style>
 
     <table class="wrapper" width="100%" cellpadding="0" cellspacing="0">
+    <tr>
+    <td style="background-color: #00aabf; padding: 15px; text-align: center;">
+        <img src="http://empazweb.org/img/logo-b.png">
+    </td>
+    </tr>
         <tr>
             <td align="center">
                 <table class="content" width="100%" cellpadding="0" cellspacing="0">
