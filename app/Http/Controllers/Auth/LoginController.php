c360991343800b0d7e1466860cec064e6cbd800a
Christian Benito <bbull.inc@gmail.com>
2018-02-13 08:10:52 -0500

    ajustes al footer

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -55,7 +55,7 @@ class LoginController extends Controller
         else
         {
             return view('auth.login')->withErrors([
-                "auth" => "Datos incorrectos. Vuelte a intentarlo."
+                "auth" => "Datos incorrectos. Vuelve a intentarlo."
             ]);
         }
     }
