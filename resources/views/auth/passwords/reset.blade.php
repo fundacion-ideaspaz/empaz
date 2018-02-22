7acdce8110d2bc5dc6d6dbc292166f8839b8871a
Christian Benito <bbull.inc@gmail.com>
2018-02-22 10:53:14 -0500

    ajustes al footer

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -15,7 +15,7 @@
                         <input type="hidden" name="token" value="{{ old('token') }}" value="{{ $token }}">
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                            <label for="email" class="col-md-4 control-label">Email</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" value="{{ $email or old('email') }}" required autofocus>
