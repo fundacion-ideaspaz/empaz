f8ab8e6f6b25f0301c9fe4dff4e55de5a078d4e5
Christian Benito <bbull.inc@gmail.com>
2018-02-22 21:36:33 -0500

    ajustes al footer

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,8 +6,8 @@
   <meta name="viewport" value="{{ old('viewport') }}" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>EmPaz - @yield('title')</title>
-  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script> 
   <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script> 
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M"
     crossorigin="anonymous">
   <link rel="stylesheet" href="/css/bootstrap-slider.css">
