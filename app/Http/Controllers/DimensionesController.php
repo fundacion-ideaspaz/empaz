26c2b2b74a92f8508c7d54593c0bfac5a5359a2f
Christian Benito <bbull.inc@gmail.com>
2018-02-26 11:21:18 -0500

    ajustes finales

--- app/Http/Controllers/DimensionesController.php
+++ app/Http/Controllers/DimensionesController.php
@@ -87,7 +87,7 @@ class DimensionesController extends Controller
             "nombre" => "required",
             "descripcion" => "required",
             "enunciados" => "required|array",
-            "estado" => "required",
+            "estado",
         ];
         $messages = array(
             'descripcion.required' => 'El campo descripción es requerido.',
