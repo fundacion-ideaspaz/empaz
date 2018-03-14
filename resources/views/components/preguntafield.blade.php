1bc1a0313b748c06006d86fcd50e951dd29bcb0e
Christian Benito <bbull.inc@gmail.com>
2018-03-13 19:10:36 -0500

    prueba

--- resources/views/components/preguntafield.blade.php
+++ resources/views/components/preguntafield.blade.php
@@ -1,6 +1,6 @@
 <li data-input-trigger>
   <label class="fs-field-label fs-anim-upper" for="respuesta-{{$pregunta->id}}">{{$pregunta->nombre}}</label>
-  <select id="{{$pregunta->id}}" class="form-control cs-select cs-skin-boxes fs-anim-lower" name="{{$pregunta->id}}"
+  <select id="r-{{$pregunta->id}}" class="form-control cs-select cs-skin-boxes fs-anim-lower" name="{{$pregunta->id}}"
     id="respuesta-{{$pregunta->id}}" @if($pregunta->isRequired($cuest_id)) required @endif>
     <option value="">Seleccione una opción...</option>
     @foreach($pregunta->opcionesRespuestas as $opcion)
