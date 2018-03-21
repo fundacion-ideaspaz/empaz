0c583e65ff8726c1c509b83baa20aeaf5239f1a8
Christian Benito <bbull.inc@gmail.com>
2018-03-21 12:55:52 -0500

    insidencias

--- resources/views/components/preguntafilled.blade.php
+++ resources/views/components/preguntafilled.blade.php
@@ -1,6 +1,6 @@
 <div class="form-group">
   <label>{{$pregunta->nombre}}</label>
-  <select class="form-control" name="{{$pregunta->id}}" id="respuesta-{{$pregunta->id}}" disabled>
+  <select class="form-control pregunta-select" name="{{$pregunta->id}}" id="respuesta-{{$pregunta->id}}" disabled>
     @foreach($pregunta->opcionesRespuestas as $opcion)
     @if(!($pregunta->isRequired($cuest_id) && $opcion->descripcion == 'No aplica'))
     <option value="{{$opcion->id}}" @if($opcion->id === $respuesta->opcion_respuesta_id) selected @endif>
