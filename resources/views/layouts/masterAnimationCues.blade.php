7708d577f10fc2086378f832e67268179e53fd05
Christian Benito <bbull.inc@gmail.com>
2018-04-16 15:05:30 -0500

    pruebas

--- resources/views/layouts/masterAnimationCues.blade.php
+++ resources/views/layouts/masterAnimationCues.blade.php
@@ -27,7 +27,7 @@
 <header class="master">
           <nav class="navbar navbar-expand-lg navbar-light bg-light">
           <a class="navbar-brand" href="/home">
-            <img src="/img/logo-b.svg" width="130">
+            <img src="/img/logo-b.png" width="130">
           </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown"
             aria-expanded="false" aria-label="Toggle navigation">
--- resources/views/layouts/masterhome.blade.php
+++ resources/views/layouts/masterhome.blade.php
@@ -31,7 +31,7 @@
 <div id="mensaje"></div>
 <header>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="/login"><img src="img/logo.svg" width="130"></a>
+  <a class="navbar-brand" href="/login"><img src="img/logo.png" width="130"></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"><i class="fa fa-bars" aria-hidden="true"></i></span>
   </button>
