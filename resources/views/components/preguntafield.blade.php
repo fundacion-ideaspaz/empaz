1d83565ebff2661d3c653c64f723d92e131281a9
Christian Benito <bbull.inc@gmail.com>
2018-04-17 13:58:59 -0500

    ajustes

--- resources/views/components/preguntafield.blade.php
+++ resources/views/components/preguntafield.blade.php
@@ -1,5 +1,6 @@
 <li data-input-trigger id="r-{{$pregunta->id}}">
   <label class="fs-field-label fs-anim-upper" for="respuesta-{{$pregunta->id}}">{{$pregunta->nombre}}</label>
+  <a class="btn btn-sm btn-primary descripcion" data-placement="bottom" href="#" data-toggle="tooltip" title="{{$pregunta->descripcion}}"><i class="fa fa-info-circle" aria-hidden="true"></i></a>
   <select  class="form-control pregunta-select cs-select cs-skin-boxes fs-anim-lower" name="{{$pregunta->id}}"
     id="respuesta-{{$pregunta->id}}" @if($pregunta->isRequired($cuest_id)) required @endif>
     <option value="">Seleccione una opción...</option>
