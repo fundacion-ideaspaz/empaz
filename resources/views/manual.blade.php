8e000e85ccb9913138f3deb39000f6400e45c98e
Christian Benito <bbull.inc@gmail.com>
2018-04-17 17:21:23 -0500

    ajustes

---
+++ resources/views/manual.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.master') @section('title', 'Acerca') @section('content')
+  <div class="row dimensiones-form">
+    <div class="card col-12">
+        <div class="card-body">
+<h2>Acerca de EMPAZ</h2>
+
+<p>La herramienta digital de medición de aportes empresariales a la paz fue desarrollada por Fundación Ideas para la Paz (FIP) y la Cámara de Comercio de Bogotá (CCB). La iniciativa nació como respuesta a la demanda expresada desde el sector privado en Colombia, de contar con un instrumento que permita validar y hacer seguimiento a las acciones empresariales que puedan prevenir la violencia y construir paz en sus áreas de operación y de inversión social. </p>
+  
+<h5>El proceso de desarrollo de EmPaz</h5>
+
+<p>EmPaz es el resultado de un ejercicio participativo de diseño y pilotaje de varios años. Se basa en lineamientos y casos documentados en la literatura disponible a nivel mundial sobre sector privado y paz y una sistematización de las mejores prácticas identificadas por la FIP y la CCB, complementadas con los aportes de un número importante de expertos y líderes empresariales colombianos, quienes revisaron y pilotearon la herramienta en sus organizaciones.</p>
+
+<h5>Contenidos</h5>
+
+<p>La herramienta digital EmPaz consta de cuestionarios –en la actualidad, se cuenta con un cuestionario General y un cuestionario para Macrosector Agroindustria (próximo a publicarse) con un promedio de 30 preguntas. Estas preguntas están asociadas a indicadores, los cuales a su vez, pertenecen a 6 dimensiones claves para lograr una paz sostenible. Estas dimensiones, que forman parte del modelo conceptual de intervención empresarial para la construcción de paz, desarrollado por la FIP, son: gestión estratégica para la paz, que abarca aspectos clave como la debida diligencia en derechos humanos, gestión de riesgos sensible al conflicto, transparencia y fomento de una cultura de legalidad, entre otros; Desarrollo económico inclusivo, Sostenibilidad ambiental, Institucionalidad y participación democrática, Capital humano y Reconciliación y convivencia.</p>
+
+<h5>¿Cómo funciona?</h5>
+<p>Después de haber registrado los datos de su empresa, usted puede escoger el cuestionario que quiere llenar. El cuestionario General EmPaz contiene las preguntas base que son relevantes para cualquier empresa, sin importar su tipo, tamaño o sector. Le recomendamos llenar este cuestionario primero, antes de llenar el cuestionario para Macrosector, que contiene preguntas adicionales adaptadas a los riesgos y oportunidades de sectores económicos específicos. Nota que a la fecha, se cuenta con un cuestionario de macrosector, que es el de agroindustria (a mediano plazo, se desarrollarán ediciones para otros macrosectores). </p>
+<p>Después de contestar todas las preguntas del cuestionario, la empresa obtiene un informe de diagnóstico, claro y conciso, con gráficas, descripciones del nivel de cumplimiento de los diferentes aspectos en prevención de conflicto y construcción de paz abordados en la herramienta EmPaz, y algunas recomendaciones generales para mejorar sus aportes a este importante propósito.</p>
+
+<p>Para más información y detalles sobre la metodología de medición, ver el manual de usuario.</p>
+
+<h5>EmPaz Premium</h5>
+<p>Adicional a EmPaz en su versión online, la FIP y la CCB desarrollaron EmPaz Premium, la versión ampliada de la herramienta y de acceso exclusivo, desarrollada para organizaciones empresariales que desean contar con un análisis personalizado y alineado con las necesidades específicas de su operación. Para esto, EmPaz Premium se basa en el acompañamiento de expertos FIP que visitan las instalaciones, realizan entrevistas con directivas, funcionarios clave y grupos de interés externos y aplican un protocolo de análisis de documentos de la empresa. Este trayecto tiene un costo, que es definido acorde a la necesidad de cada empresa.</p>
+
+<h5>Más información</h5>
+<p>Si desea recibir más información sobre EmPaz o si está interesado en explorar opciones de aplicar EmPaz Premium, por favor contactar: info@empazweb.org, o teléfono: +57 1 218 3449.</p>
+</div></div></div>
+@endsection
