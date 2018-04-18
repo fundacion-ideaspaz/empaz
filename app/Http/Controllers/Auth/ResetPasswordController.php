ba75db5c9d0f39ad6605a92d76cabcf69f59dec5
Christian Benito <bbull.inc@gmail.com>
2018-04-18 12:23:57 -0500

    ajustes

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -11,7 +11,6 @@ use App\User;
 use Validator;
 
 
-use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
 
 class ResetPasswordController extends Controller
