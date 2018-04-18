fe9e63961febc78b7c0f5978422296f335e7c11a
Christian Benito <bbull.inc@gmail.com>
2018-04-18 13:08:57 -0500

    ajustes

--- routes/web.php
+++ routes/web.php
@@ -139,7 +139,7 @@ Route::middleware(['auth'])->group(function () {
         Route::post('/cuestionarios/{id}/preguntas/{pregunta_id}', 'IndicadoresController@storePreguntas');
 
         Route::get('/password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
-        Route::post('/password/reset', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+        Route::post('/password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
 
         Route::get('/password/reset{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
         Route::post('/password/reset', 'Auth\ResetPasswordController@reset');
