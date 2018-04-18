c740f5af3d01bfa50de307d6c662b0d70cf41e0e
Christian Benito <bbull.inc@gmail.com>
2018-04-18 11:53:22 -0500

    ajustes

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Foundation\Auth\Passwords\PasswordsBroker;
 
 class ResetPasswordController extends Controller
 {
