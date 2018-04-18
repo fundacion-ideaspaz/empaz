30fa9f12869fe7101384679e81b56a5933283535
Christian Benito <bbull.inc@gmail.com>
2018-04-18 15:15:01 -0500

    ajustes

--- config/app.php
+++ config/app.php
@@ -51,7 +51,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://ec2-18-218-90-62.us-east-2.compute.amazonaws.com/'),
 
     /*
     |--------------------------------------------------------------------------
