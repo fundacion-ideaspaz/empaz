442e1d8c87ae896a3e187495fe43b61459f7086b
richardvelasquez <richard.velasquez@esinergia.co>
2018-04-23 10:56:53 -0500

    controller for answered forms

---
+++ app/Http/Controllers/ResultController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Cuestionario;
+use App\CuestionarioResult;
+use App\RespuestaCuestionario;
+use Auth;
+
+class ResultController extends Controller {
+
+    public function index($id) {
+        $cuestionarios = CuestionarioResult::where("user_id", '=',  $id)->get();     
+        
+         return view("result.index")->with([
+            "cuestionarios_result" => $cuestionarios_result
+        ]);
+    }
+
+
+}
