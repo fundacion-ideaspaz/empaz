480cc5b559f1de61bd9b637ac42256df35b88123
josescuderoh <josescuderoh@gmail.com>
2018-07-13 23:22:42 -0500

    Edited correo enviado message

--- resources/views/auth/passwords/reset_sent.blade.php
+++ resources/views/auth/passwords/reset_sent.blade.php
@@ -9,7 +9,7 @@
                 <h2>Correo enviado</h2>
                 </div>
                 <div class="panel-body">
-                  <p>A su correo hemos enviado un link para el restablecimiento de su contraseña. Por favor, revise su correo y siga las instrucciones. En caso de no recibir el correo, revise su bandeja de correo no deseado..</p>
+                  <p>A su correo hemos enviado un link para el restablecimiento de su contraseña. Por favor, revise su correo y siga las instrucciones. En caso de no recibir el correo, revise su bandeja de correo no deseado.</p>
                 </div>
             </div>
         </div>
