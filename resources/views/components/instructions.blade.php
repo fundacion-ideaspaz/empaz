022db7657a46733a1344a3a3868fd56dca9a03b4
josescuderoh <josescuderoh@gmail.com>
2018-07-30 09:42:37 -0500

    Edited instructions message

--- resources/views/components/instructions.blade.php
+++ resources/views/components/instructions.blade.php
@@ -2,17 +2,17 @@
   <div class="row">
   <div class="col-sm-12">
       <div class="instructions-box">
-        <label class="fs-anim-upper instructions-title">Instrucciones para diligenciar EmPaz:</label>
+        <label class="fs-anim-upper instructions-title">Instrucciones para diligenciar el cuestionario:</label>
         <label class="fs-anim-upper instructions">
-          <p>
-            Al finalizar el cuestionario y antes de enviarlo para evaluación, usted podrá rectificar sus respuestas y editarlas si es necesario. En cualquier momento del cuestionario usted podrá: <br>
-            <br>
-            - Usar el botón <strong>Ir a resumen</strong> para acceder al resumen de cuestionario. Desde aquí usted podrá <strong>Guardar</strong> sus respuestas o <strong>Limpiar</strong> las respuestas. <br>
-            <br>
-            - Presionar el botón <strong>Continuar</strong> para acceder a la siguiente pregunta.
-            <br>
-            - "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-          </p>
+          <p>En cada pregunta, usted encontrará descripciones de términos y conceptos con su explicación, presionando el botón i (también puede consultar la sección Glosario de EmPaz). </p>
+          <p>Para cada pregunta del cuestionario, usted podrá escoger entre tres a cinco opciones de respuesta, la que más se ajuste a la situación de su empresa.</p>
+          <p>Si no hay o no tiene acceso a los datos necesarios para responder, usted podrá escoger la opción “No hay información”. Tenga en cuenta que esta respuesta se valora con la calificación mínima de 1 punto.</p>
+          <p>Presionando el botón Continuar, usted accederá a la siguiente pregunta.</p>
+
+          <p>Todas las preguntas son de respuesta obligatoria. Solo cuando usted ha constado el cuestionario completo, puede enviarlo para que la herramienta EmPaz genere su informe de diagnóstico con recomendaciones. </p>
+          <p>Al finalizar el cuestionario y antes de enviarlo para evaluación, usted podrá rectificar sus respuestas y editarlas si es necesario.</p>
+          <p>En cualquier momento del cuestionario usted podrá usar el botón Ir a confirmar respuestas para acceder al resumen del cuestionario. Desde aquí usted podrá Guardar sus respuestas o Limpiar las respuestas.</p>
+          <p>Si es necesario, usted podrá salir de la plataforma para volver a diligenciar el cuestionario en otro momento. Recuerde que en este caso es importante en la página de Confirmar respuestas, presionar el botón Guardar para que la herramienta guarde el formulario con las respuestas diligenciadas hasta el momento, para su siguiente sesión (nota que sin guardar, se perderán las respuestas seleccionadas). </p>
         </label>
       </div>
     </div>
