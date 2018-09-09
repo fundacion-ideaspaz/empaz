6175745fff88ab42a91456b49c9a115a199ced47
josescuderoh <josescuderoh@gmail.com>
2018-09-09 17:29:55 -0500

    Added code for google analytics of CCB and FIP

--- resources/views/layouts/masterAnimationCues.blade.php
+++ resources/views/layouts/masterAnimationCues.blade.php
@@ -22,9 +22,38 @@
   <script src="/js/modernizr.custom.js"></script>
     <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto+Mono:100,100i,300,300i,400,400i,500,500i,700,700i|Roboto+Slab:100,300,400,700|Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i" rel="stylesheet">
 
+    <!-- Analytics for both parties -->
+
+    <!--  Google Analytics for FIP -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-125477076-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'UA-125477076-1');
+    </script>
+
+    <!-- Google Tag Manager for CCB-->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-K45NWW');
+    </script>
+    <!-- End Google Tag Manager -->
+
+
+
 </head>
 
 <body>
+
+  <!-- Google Tag Manager (noscript) -->
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K45NWW"
+  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
+
+
   <header class="master">
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
           <a class="navbar-brand" href="/home">
--- resources/views/layouts/masterTablero.blade.php
+++ resources/views/layouts/masterTablero.blade.php
@@ -24,7 +24,36 @@
 <script src="/js/fusioncharts.js"></script>
 <script src="/js/fusioncharts.powercharts.js"></script>
 <script src="/js/jspdf.min.js"></script>
+
+<!-- Analytics for both parties -->
+
+<!--  Google Analytics for FIP -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-125477076-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'UA-125477076-1');
+</script>
+
+<!-- Google Tag Manager for CCB-->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-K45NWW');
+</script>
+<!-- End Google Tag Manager -->
+
+
+
 </head>
+
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K45NWW"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
 
 <body class="tablero">
   <header class="master">
